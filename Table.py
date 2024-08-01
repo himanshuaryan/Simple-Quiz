d8d7de75dad070417c8c31c68668c82b8987702d
H I M A N S H U <119969631+himanshuaryan@users.noreply.github.com>
2024-08-02 01:07:18 +0530

    Update Table.py

--- Table.py
+++ Table.py
@@ -1,5 +1,5 @@
 print("code by @himanshuaryan".center(110))
 n = int(input("Enter number : "))
-table = [x for x in range(1,10)]
+table = [x for x in range(1,11)]
 for x in table:
   print(x,"*",n,"=",x*n)
