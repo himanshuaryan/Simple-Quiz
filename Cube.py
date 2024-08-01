732836f1e6ccd1ac6e1adc5923c9891923ad2d2b
H I M A N S H U <119969631+himanshuaryan@users.noreply.github.com>
2024-08-02 01:12:53 +0530

    Update Cube.py

--- Cube.py
+++ Cube.py
@@ -2,5 +2,6 @@ print("code by @himanshuaryan".center(110))
 n = int(input("Enter number : "))
 print(" ")
 table = [x for x in range(1,n+1)]
+print("The cube of first nth numbers are : \n")
 for x in table:
-    print(f"{x} * {x} = {x**3}")
+    print(f"\t{x} * {x} = {x**3}")
